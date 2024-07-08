be62fcbc3861b44ada2db8ead2d0a7a098bd5c9a
Mike Street <mikestreety@gmail.com>
2024-07-08 12:24:44 +0100

    feat: Dynamically add "please select" if there is not 1 item

--- Classes/Backend/ItemsProcFunc.php
+++ Classes/Backend/ItemsProcFunc.php
@@ -18,6 +18,14 @@ class ItemsProcFunc
 		$engines = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['media_gallery']['engines'] ?? [];
 		ksort($engines);
 
+		// If there is _only_ one, we don't want the "please select" option
+		if (count($engines) !== 1) {
+			$config['items'][] = [
+				'LLL:EXT:media_gallery/Resources/Private/Language/locallang_tca.xlf:media_gallery.flexform.please_select',
+				'',
+			];
+		}
+
 		foreach ($engines as $key => $value) {
 			$config['items'][] = [$value['title'], $key];
 		}
